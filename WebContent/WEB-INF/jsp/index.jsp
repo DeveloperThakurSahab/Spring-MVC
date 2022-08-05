d69e63d758c20b9e0c949fc94ce1c411fbf33174
Rajput Ankush <mrrajput6780@gmail.com>
2022-08-05 20:53:01 +0530

    first commit

---
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Index Page</title>
+</head>
+<body>
+<a href="hello">Hello</a>
+<br><br>
+<a href="welcome">Welcome</a>
+</body>
+</html>
---
+++ src/com/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() 
+	{
+		return "index";
+	}
+	
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String hello() 
+	{
+		return "hello";
+	}
+	
+	@RequestMapping(value = "welcome", method = RequestMethod.GET)
+	public String welcome() 
+	{
+		return "welcome";
+	}
+}
